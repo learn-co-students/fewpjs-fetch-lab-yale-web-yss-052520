65ef0e671c2614b8c514cc059a99e960d87e6107
Drew Beckmen <drew.beckmen@yale.edu>
2020-07-07 16:57:31 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function fetchBooks() {
-
+  return fetch("https://anapioficeandfire.com/api/books").then(function(response) {return response.json()}).then(function(json) {renderBooks(json)})
 }
 
 function renderBooks(books) {
